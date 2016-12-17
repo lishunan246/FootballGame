40256595ef89581c99e79b99a3805d2fa0b7def7
Li Shunan <lishunan246@gmail.com>
2016-12-17 15:20:33 +0800

    init

---
+++ Assets/Controller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private CharacterController _myController;
+    public float gravity = 9.81f;
+
+    // public variables
+    public float moveSpeed = 3.0f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        // store a reference to the CharacterController component on this gameObject
+        // it is much more efficient to use GetComponent() once in Start and store
+        // the result rather than continually use etComponent() in the Update function
+        _myController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Determine how much should move in the z-direction
+        var movementZ = Input.GetAxis("Vertical")*Vector3.forward*moveSpeed*Time.deltaTime;
+
+        // Determine how much should move in the x-direction
+        var movementX = Input.GetAxis("Horizontal")*Vector3.right*moveSpeed*Time.deltaTime;
+
+        // Convert combined Vector3 from local space to world space based on the position of the current gameobject (player)
+        var movement = transform.TransformDirection(movementZ + movementX);
+
+        // Apply gravity (so the object will fall if not grounded)
+        movement.y -= gravity*Time.deltaTime;
+
+        Debug.Log("Movement Vector = " + movement);
+
+        // Actually move the character controller in the movement direction
+        _myController.Move(movement);
+    }
+}
---
+++ Assets/Damage.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public bool continuousDamage = false;
+    public float continuousTimeBetweenHits = 0;
+
+    public float damageAmount = 10.0f;
+    public bool damageOnCollision = false;
+
+    public bool damageOnTrigger = true;
+    public float delayBeforeDestroy = 0.0f;
+
+    public bool destroySelfOnImpact = false; // variables dealing with exploding on impact (area of effect)
+    public GameObject explosionPrefab;
+
+    private float savedTime;
+
+    private void OnTriggerEnter(Collider collision) // used for things like bullets, which are triggers.
+    {
+        if (damageOnTrigger)
+        {
+            if ((tag == "PlayerBullet") && (collision.gameObject.tag == "Player"))
+                // if the player got hit with it's own bullets, ignore it
+                return;
+
+            if (collision.gameObject.GetComponent<Health>() != null)
+            {
+                // if the hit object has the Health script on it, deal damage
+                collision.gameObject.GetComponent<Health>().ApplyDamage(damageAmount);
+
+                if (destroySelfOnImpact)
+                    Destroy(gameObject, delayBeforeDestroy); // destroy the object whenever it hits something
+
+                if (explosionPrefab != null)
+                    Instantiate(explosionPrefab, transform.position, transform.rotation);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+        // this is used for things that explode on impact and are NOT triggers
+    {
+        if (damageOnCollision)
+        {
+            if ((tag == "PlayerBullet") && (collision.gameObject.tag == "Player"))
+                // if the player got hit with it's own bullets, ignore it
+                return;
+
+            if (collision.gameObject.GetComponent<Health>() != null)
+            {
+                // if the hit object has the Health script on it, deal damage
+                collision.gameObject.GetComponent<Health>().ApplyDamage(damageAmount);
+
+                if (destroySelfOnImpact)
+                    Destroy(gameObject, delayBeforeDestroy); // destroy the object whenever it hits something
+
+                if (explosionPrefab != null)
+                    Instantiate(explosionPrefab, transform.position, transform.rotation);
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) // this is used for damage over time things
+    {
+        if (continuousDamage)
+            if ((collision.gameObject.tag == "Player") && (collision.gameObject.GetComponent<Health>() != null))
+                if (Time.time - savedTime >= continuousTimeBetweenHits)
+                {
+                    savedTime = Time.time;
+                    collision.gameObject.GetComponent<Health>().ApplyDamage(damageAmount);
+                }
+    }
+}
