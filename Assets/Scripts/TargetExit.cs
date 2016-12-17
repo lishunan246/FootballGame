0ba985630fe9dd0462c2591e76565d7cd5ee27e7
Li Shunan <lishunan246@gmail.com>
2016-12-17 18:14:44 +0800

    add bullet

---
+++ Assets/Scripts/TargetExit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class TargetExit : MonoBehaviour
+{
+    public float exitAfterSeconds = 10f; // how long to exist in the world
+    public float exitAnimationSeconds = 1f; // should be >= time of the exit animation
+
+    private bool startDestroy;
+    private float targetTime;
+
+    // Use this for initialization
+    private void Start()
+    {
+        // set the targetTime to be the current time + exitAfterSeconds seconds
+        targetTime = Time.time + exitAfterSeconds;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // continually check to see if past the target time
+        if (Time.time >= targetTime)
+            if (GetComponent<Animator>() == null)
+                // no Animator so just destroy right away
+                Destroy(gameObject);
+            else if (!startDestroy)
+            {
+                // set startDestroy to true so this code will not run a second time
+                startDestroy = true;
+
+                // trigger the Animator to make the "Exit" transition
+                GetComponent<Animator>().SetTrigger("Exit");
+
+                // Call KillTarget function after exitAnimationSeconds to give time for animation to play
+                Invoke("KillTarget", exitAnimationSeconds);
+            }
+    }
+
+    // destroy the gameObject when called
+    private void KillTarget()
+    {
+        // remove the gameObject
+        Destroy(gameObject);
+    }
+}
