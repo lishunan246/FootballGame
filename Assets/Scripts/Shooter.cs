80435efac8dda4e7eff8fff40743c344fc429c40
ZJUZT <3120101111@zju.edu.cn>
2017-01-07 12:52:46 +0800

    dribble ball

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,20 +5,28 @@ public class Shooter : MonoBehaviour
 {
     public Slider ForceBarSlider;
     private float _kickForce;
+    private GameObject _Football;
 
     private readonly float MaxKickForce = 1.0f;
+    private readonly float baseKickForce = 20.0f;
+
     public float force=300.0f;
     public float up = 1.0f;
     public float power = 10.0f;
 
     // dribble
-    public float dribble_range = 5.0f;
+    public float dribbleRange = 5.0f;
+    public Vector3 preForward = new Vector3(0.0f,0.0f,0.0f);
 
     // Reference to projectile prefab to shoot
     public GameObject projectile;
 
     // Reference to AudioClip to play
     public AudioClip shootSFX;
+
+    private void Start(){
+        _Football = GameObject.FindWithTag("Football");
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -29,7 +37,7 @@ public class Shooter : MonoBehaviour
 
             f[1] = up;
 
-            ball.GetComponent<Rigidbody>().AddForce(f*(_kickForce+0.2f)*force);
+            ball.GetComponent<Rigidbody>().AddForce(f*(_kickForce+baseKickForce)*force);
             GameManager.gm.LastBallTouch=GameManager.Side.Player;
             _kickForce = 0;
         }
@@ -38,16 +46,23 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
-        if(Input.GetButton("Fire1")){
-            GameObject ball = GameObject.FindWithTag("Football");
-            float dist = Vector3.Distance (gameObject.transform.position, ball.transform.position);
-            // Debug.Log (string.Format ("Distance between {0} and {1} is: {2}", gameObject, ball, dist));
-            if(dist<=dribble_range){
+        // GameObject ball = GameObject.FindWithTag("Football");
+        float dist = Vector3.Distance (gameObject.transform.position, _Football.transform.position);
+        // move the ball in horizon
+        if(dist<=dribbleRange){
+            Vector3 forward = gameObject.transform.forward;
+            _Football.GetComponent<Rigidbody>().AddForce((forward-preForward)*baseKickForce);
+            Debug.Log(string.Format("Applied force....{0}",(forward-preForward)*baseKickForce));
+            // sleep the ball
+            if(Input.GetButton("Fire1")){
                 // stop the ball
-                ball.GetComponent<Rigidbody>().Sleep();
+                _Football.GetComponent<Rigidbody>().Sleep();     
             }
         }
+        else{
+            preForward = new Vector3(0.0f,0.0f,0.0f);
+        }
+        
 
         if (Input.GetButton("Jump"))
         {
