f317787c9f21410d44329a30b3bdf18b3b2e0a3b
Li Shunan <lishunan246@gmail.com>
2017-01-07 16:09:40 +0800

    keep

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -16,14 +16,16 @@ public class AI : MonoBehaviour
     private Vector3 _tempDestination;
     public float AngleCosMax = 0.8f;
     public Stratagy AI_Stratagy = Stratagy.Pass;
-    public float BallDistance = 1.0f;
+    public float BallDistance = 1.5f;
     public float PassForce = 10.0f;
     public float ShootForce = 300.0f;
+    public float ShootDistance = 20.0f;
     public GameManager.Side side = GameManager.Side.Computer;
     public float Speed = 3.0f;
     public Vector3 TargetGoal = 55 * Vector3.back;
     public float up = 1.0f;
     public float X_MAX;
+    public float distanceToGoal;
 
     private void Start()
     {
@@ -49,26 +51,33 @@ public class AI : MonoBehaviour
         var transformForward = pos - gameObject.transform.position;
         transformForward.y = 0;
         gameObject.transform.forward = transformForward;
-        gameObject.transform.position += transformForward.normalized * Speed * Time.deltaTime;
+        var rb = gameObject.GetComponent<Rigidbody>();
+        rb.MovePosition(gameObject.transform.position + transformForward.normalized * Speed * Time.deltaTime);
     }
 
+    private bool NearTargetGoal()
+    {
+        
+        return distanceToGoal < ShootDistance;
+    }
     // Update is called once per frame
     private void Update()
     {
+        distanceToGoal = (TargetGoal - gameObject.transform.position).magnitude;
         if (DistanceToBall() > BallDistance + 0.2)
         {
             MoveBehind(_tempDestination);
         }
         else
         {
-            if (Vector3.Dot(gameObject.transform.forward.normalized, (TargetGoal - gameObject.transform.position).normalized) > AngleCosMax)
+            if (NearTargetGoal()||Vector3.Dot(gameObject.transform.forward.normalized, (TargetGoal - gameObject.transform.position).normalized) > AngleCosMax)
             {
                 MoveTo(_ball.transform.position);
             }
             else
             {
                 var t = Vector3.Dot(gameObject.transform.forward, Vector3.right)>0?1:-1;
-                
+                var rb = gameObject.GetComponent<Rigidbody>();
                 gameObject.transform.RotateAround(gameObject.transform.position, Vector3.up, 180 * Time.deltaTime*t);
                 _ball.transform.RotateAround(gameObject.transform.position, Vector3.up, 180 * Time.deltaTime*t);
             }
@@ -90,46 +99,28 @@ public class AI : MonoBehaviour
         {
             _ball = collision.gameObject;
             var distance = (_ball.transform.position - gameObject.transform.position).magnitude;
+            
             var kickDirection = gameObject.transform.forward;
 
             kickDirection[1] = up;
             var kickForce = (float) rnd.NextDouble();
             Vector3 tf;
-            if (AI_Stratagy == Stratagy.Shoot)
+            if (distanceToGoal<ShootDistance)
+            {
+                kickDirection = (TargetGoal - gameObject.transform.position).normalized;
+                kickDirection[1] = up;
+                tf = kickDirection * (kickForce + 0.2f) * ShootForce;
+            }
+            else if (AI_Stratagy==Stratagy.Shoot)
             {
                 tf = kickDirection * (kickForce + 0.2f) * ShootForce;
-                if (Vector3.Dot(kickDirection, TargetGoal) < 0)
-                    if (_ball.transform.position.x > 0)
-                    {
-                        var turnRight = Quaternion.FromToRotation(Vector3.forward, Vector3.left);
-                        tf = turnRight * tf;
-                    }
-                    else
-                    {
-                        var turnRight = Quaternion.FromToRotation(Vector3.forward, Vector3.right);
-                        tf = turnRight * tf;
-                    }
+                
             }
             else
             {
-                kickDirection[1] = 0.01f;
-                tf = kickDirection * 0.2f * PassForce;
-                if (Vector3.Dot(kickDirection, TargetGoal) < 0)
-                {
-                    tf = kickDirection * (kickForce + 0.2f) * ShootForce / 2;
-                    if (_ball.transform.position.x > 0)
-                    {
-                        var turnRight = Quaternion.FromToRotation(Vector3.forward, Vector3.left);
-                        tf = turnRight * tf;
-                    }
-                    else
-                    {
-                        var turnRight = Quaternion.FromToRotation(Vector3.forward, Vector3.right);
-                        tf = turnRight * tf;
-                    }
-                }
+                kickDirection[1] = 0.5f;
+                tf = kickDirection * 1.0f * PassForce;
             }
-
 
             _ball.GetComponent<Rigidbody>().AddForce(tf);
             GameManager.gm.LastBallTouch = side;
